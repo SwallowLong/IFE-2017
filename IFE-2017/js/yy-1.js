754cc404a40282ca482ef51746237cd55b4ffc92
SwallowLong <1679187550@qq.com>
2017-07-01 21:38:25 +0800

    yaoyaoxueyuan

--- IFE-2017/js/yy-1.js
+++ IFE-2017/js/yy-1.js
@@ -2,19 +2,26 @@ btn.addEventListener( "click", function() {
 	var btn = document.getElementById("btn"),
 	input = document.getElementsByClassName("t")
 	bottom = document.getElementsByClassName("bottom");
-	var re = new RegExp ("[\\u4E00-\\u9FFF0-9a-zA-Z]+","g");
-    var myValue = input[0].value.replace(/[\u4E00-\u9FFF]/g,"aa");
-	if ( myValue.length >= 4 && myValue.length <= 14) {
-		bottom[0].innerHTML = "名称格式正确"; 
-		bottom[0].id = "green";
-		input[0].id = "tgreen";
-	} else if ( myValue.length == 0 ) {
-		bottom[0].innerHTML = "姓名不能为空"; 
-		bottom[0].id = "red"
+	var re = new RegExp ("[^\\x00-\\xff]+","g");
+	var reChina = new RegExp ("[\\u4E00-\\u9FFF]+","g");
+	if ( re.test(input[0].value) && !reChina.test(input[0].value) ) {
+		bottom[0].innerHTML = "不能输入中文符号"; 
+		bottom[0].id = "red1"
 		input[0].id = "tred";
 	} else {
-		bottom[0].innerHTML = "请输入4~14个字符";
-		bottom[0].id = "gray";
-		input[0].id = "tgary";
+		var myValue = input[0].value.replace(/[\u4E00-\u9FFF]/g,"aa");
+		if ( myValue.length >= 4 && myValue.length <= 14) {
+			bottom[0].innerHTML = "名称格式正确"; 
+			bottom[0].id = "green";
+			input[0].id = "tgreen";
+		} else if ( myValue.length == 0 ) {
+			bottom[0].innerHTML = "姓名不能为空"; 
+			bottom[0].id = "red"
+			input[0].id = "tred";
+		} else {
+			bottom[0].innerHTML = "请输入4~14个字符";
+			bottom[0].id = "gray";
+			input[0].id = "tgary";
+		}
 	}
 } )
