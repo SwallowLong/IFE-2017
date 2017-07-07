7b7ab69c18a0b833a962d4028a43f15714a39540
SwallowLong <1679187550@qq.com>
2017-07-07 19:49:02 +0800

    nmxueyuan

--- IFE-2017/js/nm-lbt.js
+++ IFE-2017/js/nm-lbt.js
@@ -1,15 +1,29 @@
+(function() {
 var img = document.getElementsByTagName("img"),
 box = document.getElementById("box"),
 btn = document.getElementById("btn");
+var j = 0,
+x = 0;
+//每张图片添加点击事件
 for (var i=0; i<img.length; i++) {
 	img[i].addEventListener( "click", function() {
-		box.className = "start";
-
-		/*setTimeout( function() {
-			box.className = "start";
-		}, 0 )
-		setTimeout( function() {
-			box.style = "animation-play-state: paused; -moz-animation-play-state: paused; -webkit-animation-play-state: paused;";
-		}, 1000 )*/
+       //根据点击选择是循环还是过渡模式
+       if (j==0) {
+            box.className = "start";
+            j=1;
+       } else {            
+            box.className = "start1";
+            box.style = " transform:rotateY(" + -60*x + "deg);-webkit-transform:rotateY(" + -60*x + "deg);";
+        //由于条件语句在rotate后 所以其实x=6也是会先执行一次
+        //然后条件语句在else
+        if (x<6) {
+            x++;
+        } else {
+            x=0;
+            j=0;
+        }
+       } 
 	}, false )
 }
+})();
+    
